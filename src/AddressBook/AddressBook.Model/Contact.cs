9504ffdd8725aa04255b5b474cb57f378f13cc56
Ivan <kiselevi341@gmail.com>
2022-06-03 15:11:22 +0700

    fix(PgoneNumber): Исправлено форматирование и несогласованность доступа к классам

--- src/AddressBook/AddressBook.Model/Contact.cs
+++ src/AddressBook/AddressBook.Model/Contact.cs
@@ -8,5 +8,16 @@ namespace AddressBook.Model
 {
     public class Contact
     {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Patronymic { get; set; }
+
+        public PhoneNumber Number { get; set; }
+
+        public ProjectSerializer projectSerializer;
     }
 }
--- src/AddressBook/AddressBook.Model/ProjectSerializer.cs
+++ src/AddressBook/AddressBook.Model/ProjectSerializer.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace AddressBook.Model
 {
-    internal class ProjectSerializer
+    public class ProjectSerializer
     {
     }
 }
