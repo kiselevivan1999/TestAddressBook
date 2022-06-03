0bb038ae06822ef654ca384c882a57ef1eef748d
Ivan <kiselevi341@gmail.com>
2022-06-03 15:50:10 +0700

    features(ProjectSerializer): Добавлена основная логика. Не рабочая

--- src/AddressBook/AddressBook.Model/ProjectSerializer.cs
+++ src/AddressBook/AddressBook.Model/ProjectSerializer.cs
@@ -3,10 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
 
 namespace AddressBook.Model
 {
-    public class ProjectSerializer
+    public static class ProjectSerializer
     {
+        private static readonly string _fileName = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\AddressBook\userdata.xml";
+
+        private static XmlDocument xmlDoc = new XmlDocument();
+
+        public static void SaveToFile(Contact contact) 
+        {
+            CreateFile();
+
+            XmlElement newElem = xmlDoc.CreateElement("price");
+            newElem.InnerText = "10.95";
+            xmlDoc.DocumentElement.AppendChild(newElem);
+
+
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent = true;
+
+            
+            XmlWriter writer = XmlWriter.Create("data.xml", settings);
+            xmlDoc.Save(writer);
+
+        }
+
+        public static Contact LoadFromFile()
+        {
+            CreateFile();
+
+            return null;
+        }
+
+
+        private static void CreateFile() 
+        {
+            if (File.Exists(_fileName)) 
+            {
+                return;
+            }
+
+            Directory.CreateDirectory(_fileName.Replace("\\user.xml", ""));
+
+            using (FileStream fs = File.Create(_fileName)) 
+            {
+                fs.Dispose();
+            }
+        }
     }
 }
