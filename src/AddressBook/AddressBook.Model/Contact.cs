360a4c11c4d4bd917f0bee95b062a6f07f13da6f
Ivan <kiselevi341@gmail.com>
2022-06-07 11:05:01 +0700

    features(Contact): Добавление XML-комментариев.

--- src/AddressBook/AddressBook.Model/Contact.cs
+++ src/AddressBook/AddressBook.Model/Contact.cs
@@ -2,24 +2,41 @@
 
 namespace AddressBook.Model
 {
+    /// <summary>
+    /// Описывает контакт.
+    /// </summary>
     public class Contact
     {
-
+        /// <summary>
+        /// Имя контакта.
+        /// </summary>
         private string _name;
 
+        /// <summary>
+        /// Фамилия контакта.
+        /// </summary>
         private string _sername;
 
+        /// <summary>
+        /// Отчество контакта.
+        /// </summary>
         private string _patronymic;
 
+        /// <summary>
+        /// ID контакта.
+        /// </summary>
         public int Id { get; set; }
         
+        /// <summary>
+        /// Возвращает и задаёт имя.
+        /// </summary>
         public string Name 
         {
             get => _name;
 
             set 
             {
-                if (!isCorrectLenght(value)) 
+                if (!isValidated(value)) 
                 {
                     throw new ArgumentException("Name must be between 2 and 50 characters");
                 }
@@ -27,13 +44,16 @@ namespace AddressBook.Model
             } 
         }
 
+        /// <summary>
+        /// Возвращает и задаёт фамилию.
+        /// </summary>
         public string Surname 
         {
             get => _sername;
 
             set
             {
-                if (!isCorrectLenght(value))
+                if (!isValidated(value))
                 {
                     throw new ArgumentException("Sername must be between 2 and 50 characters");
                 }
@@ -41,13 +61,16 @@ namespace AddressBook.Model
             }
         }
 
+        /// <summary>
+        /// Возвращает и задаёт отчество.
+        /// </summary>
         public string Patronymic 
         {
             get => _patronymic;
 
             set
             {
-                if (!isCorrectLenght(value))
+                if (!isValidated(value))
                 {
                     throw new ArgumentException("Patronymic must be between 2 and 50 characters");
                 }
@@ -55,11 +78,17 @@ namespace AddressBook.Model
             }
         }
 
+        /// <summary>
+        /// Возвращает и задаёт номер телефона
+        /// </summary>
         public PhoneNumber Number { get; set; }
 
-
-
-        private bool isCorrectLenght(string str) 
+        /// <summary>
+        /// Проверяет строку на соответствие формату.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns>true - строка подходит по требованиям, false - строка не прошла проверку.</returns>
+        private bool isValidated(string str) 
         {
             if (str.Length >= 2 && str.Length <= 50) 
             {
